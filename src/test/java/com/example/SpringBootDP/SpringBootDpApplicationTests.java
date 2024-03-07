1715c0f5b5aa9d8813fafc1e6fda570ae06f2b6b
tomaskomora <xkomorat@stuba.sk>
2024-03-07 22:53:58 +0100

    init

---
+++ src/test/java/com/example/SpringBootDP/SpringBootDpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootDP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
